81bbbaa0cffa91849fcfe609ccaeab5069ed9469
ralexander <theravenalexander@gmail.com>
2018-11-04 17:46:11 -0700

    almost

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', FALSE);
+
+SELECT * FROM burgers_db.burgers;
